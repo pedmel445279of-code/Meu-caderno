20019807ba162b9b5ec48a639a481269bc2bdbb2
Pedrin_ Pistoleiro <phms7974@gmail.com>
2026-08-13 09:08:05 -0300

    Mini projeto

--- Projetos/Projeto caixa de mercado/README.md
+++ Projetos/Projeto caixa de mercado/README.md
@@ -1,10 +1,11 @@
 Proposta:
 
-Eu quero que voce fala um programa aonde eu clico no produto e ele é adicionado ao carrinho, e quando eu terminar de comprar eu faço um cadastro e quando terminar de fazer o cadastro eu consiga ver tudo o que eu pedi e todos os meus dados como se fosse uma notinha.
+Eu quero que voce faça um programa aonde eu clico no produto e ele é adicionado ao carrinho, e quando eu terminar de comprar eu faço um cadastro e quando terminar de fazer o cadastro eu consiga ver tudo o que eu pedi e todos os meus dados como se fosse uma notinha.
 
 Logica:
 
 -Ao clicar no botao vou adicionar um item no carrinho;
+    -O item tem que ser indentificado, ou seja temos que saber qual item foi adicionado e quantos itens foram pro carrinho;
 -Quando clica em cadastro, leva a uma tela com um formulario pra voce preencher;
--Clicar em enviar, te lava a uma pagina te mostrando tudo o que voce pedio e seus dados
+-Clicar em enviar, te lava a uma pagina te mostrando tudo o que voce pedio e seus dados;
     -Nesse caso voce terá que pegar todos os dados que voce preencheu e enviar para uma nova pagina, tente usar o documento.getElementID() e o documento.write!
--- Projetos/Projeto caixa de mercado/logica.js
+++ Projetos/Projeto caixa de mercado/logica.js
@@ -1,18 +1,38 @@
-/*let produtos = ['Arroz','Leite','Custela','Frango','Oleo','Miojo',]*/
+
+//LOJA
+    //adiciona mais um item no carrinho (è ele quem conta quantos item tem no carrinho)
+    let mais = 0
+    let produtos = []
+        //Adiciona um item em especifico no carrinho
+        function produtosEspecificos(produto){
+        produtos.push(produto)
+            //Nesse trecho ele esta contando quantos itens tem
+        mais++
+    document.getElementById('contadorProdutos').textContent = mais
+    }
+        console.log(produtos)
+//CADASTRO
+    function dadosCadastro(){
+        let nome = document.getElementById('name').value
+        let CPF = document.getElementById('CPF').value
+        let data = document.getElementById('data').value
+        let celular = document.getElementById('numero').value
+        let CEP = document.getElementById('CEP').value
+        let Rua = document.getElementById('rua').value
+        let casa = document.getElementById('casa').value
+        let complemento = document.getElementById('complemento').value
+        document.write("--Dados do Cliente--" +' <br></br>')
+        document.write("Nome: " + nome + ' <br></br>')
+        document.write("CPF: " + CPF)
+        document.write("Data de nacimento" + data+ ' <br></br>')
+        document.write("Número de celular: " + celular+' <br></br>')
+        document.write("CEP da casa: " + CEP+' <br></br>')
+        document.write("Nome da rua: " + Rua+' <br></br>')
+        document.write("Número da casa: " + casa+' <br></br>')
+        document.write("Complemento: " + complemento+' <br></br>')
+    }
+dadosCadastro()
+    
 
 
-    //adiciona mais um item no carrinho
-let mais = 0
-function adicionar(){
-    mais++
-   document.getElementById('contadorProdutos').textContent = mais
-}
 
-    //enviar os dados para outra pagina:
-function enviarFormulario(){
-
-    let dados = document.getElementById('container').value
-
-}
-
-enviarFormulario()
