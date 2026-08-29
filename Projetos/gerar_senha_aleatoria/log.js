63f77b6321fe3c78139f45ae288993cc867af260
Pedrin_ Pistoleiro <phms7974@gmail.com>
2026-08-29 12:37:50 -0300

    complemento do projeto senha

--- Projetos/gerar_senha_aleatoria/log.js
+++ Projetos/gerar_senha_aleatoria/log.js
@@ -1,15 +1,25 @@
 function gerarSenhar(){
-        let quantCaracter= document.getElementById('tamanhoSenha').value
-        let caracteres = "0132456789abcdefghijklmnopqrstuvwxyz_/;:~|"
-        let senha = []
+    let quantCaracter= document.getElementById('tamanhoSenha').value
+    let caracteres = "0132456789abcdefghijklmnopqrstuvwxyz_/;:~|"
+    let senha = []
        
         caracterSeparado = caracteres.split('')
        
-        for(let i = 1;i<=quantCaracter;++i){  //Repetir uma ação 10 vezes e, em cada repetição, gerar um número inteiro aleatório entre 0 e 9.
+    for(let i = 1;i<=quantCaracter;++i){  //Repetir uma ação 10 vezes e, em cada repetição, gerar um número inteiro aleatório entre 0 e 9.
+        
         let aleatoria = Math.floor(Math.random()*caracterSeparado.length) //Aqui ele vai pegar apenas um elemento do Array(quem vai pegar os outros vai ser o for).
+       
         let senhaGuardada = caracterSeparado[aleatoria] //Está guantando em formato de Array a letras/numeros sortidas
+        
         senha.push(senhaGuardada) // com o push ele adiciona ao final do array as letras/numeros sortidas.
         }
-        let senhaFinal = senha.join('')
+    let senhaFinal = senha.join('')
         document.getElementById('newSenha').textContent = senhaFinal
     }
+
+    function dadosTela(){
+        let largura = window.innerHeight
+        let altura = window.innerWidth
+        console.log(largura, altura)
+    }
+    dadosTela()
